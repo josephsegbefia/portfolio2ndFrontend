369d89e73145ee18466c359bbec335e7c8320ec7
Joseph Segbefia <elorma.segbefia@gmail.com>
2023-08-12 19:08:54 +0200

    Add Create profile function to dashboard

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -4,6 +4,7 @@ import CreateProject from "./CreateProject";
 import ProjectList from "./ProjectList";
 import MessagesList from "./MessagesList";
 import JobList from "./JobList";
+import CreateProfile from "./CreateProfile";
 
 const Dashboard = () => {
   const [activeButton, setActiveButton] = useState(null);
@@ -22,6 +23,10 @@ const Dashboard = () => {
 
   const openJobsTable = () => {
     setActiveButton("jobs");
+  };
+
+  const openCreateProfile = () => {
+    setActiveButton("profile");
   };
 
   return (
@@ -59,6 +64,11 @@ const Dashboard = () => {
           <JobList />
           <button onClick={() => setActiveButton(null)}>Cancel</button>
         </>
+      ) : activeButton === "profile" ? (
+        <>
+          <CreateProfile />
+          <button onClick={() => setActiveButton(null)}>Cancel</button>
+        </>
       ) : (
         <>
           <button
@@ -89,6 +99,13 @@ const Dashboard = () => {
             className="focus:outline-none text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5  mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 m-10"
           >
             View Jobs
+          </button>
+          <button
+            type="button"
+            onClick={openCreateProfile}
+            className="focus:outline-none text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5  mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 m-10"
+          >
+            Create profile
           </button>
         </>
       )}
--- src/components/ProjectDetails.jsx
+++ src/components/ProjectDetails.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import axios from "axios";
-
+import { Link } from "react-router-dom";
+import { FaGithub, FaExternalLinkSquareAlt } from "react-icons/fa";
 const API_URL = "http://localhost:5005";
 const ProjectDetails = () => {
   const [project, setProject] = useState([]);
@@ -21,7 +22,50 @@ const ProjectDetails = () => {
   }, [projectId]);
 
   console.log(project);
-  return <div className="flex min-h-screen"></div>;
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="w-1/2 bg-white p-8 shadow-md rounded-lg flex">
+        <div className="w-1/3 pr-4">
+          <img
+            src={project.imageUrl}
+            alt="Card Image"
+            className="h-auto w-full rounded-lg"
+          />
+        </div>
+        <div className="w-1/2">
+          <h2 className="text-2xl font-semibold mb-4">{project.projectName}</h2>
+          <p className="text-gray-600">{project.description}</p>
+          <div className="justify-center grid gap-4 lg:gap-3 lg:grid-cols-5 mt-5 md:grid-cols-3 sm:grid-cols-2 overflow-scroll">
+            <ol>
+              {project.techs &&
+                project.techs.map((tech, index) => (
+                  <li
+                    className="text-gray-900 dark:bg-teal-600 bg-rose-600 rounded-md p-1.5 dark:text-white w-fit m-1"
+                    key={index}
+                  >
+                    {tech}
+                  </li>
+                ))}
+            </ol>
+          </div>
+          <div className="px-6 py-4 flex">
+            <a
+              className="text-2xl cursor-pointer duration-150 hover:scale-110 m"
+              href={project.githubUrl}
+            >
+              <FaGithub />
+            </a>
+            <a
+              className="text-2xl cursor-pointer duration-150 hover:scale-110"
+              href={project.demoLink}
+            >
+              <FaExternalLinkSquareAlt />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProjectDetails;
